39f2ba3f45fd5d61d20b9fa87af1c992c6805519
elise <115508722+cultelise@users.noreply.github.com>
2022-10-20 06:13:51 -0500

    Roll eyes at file jumping to finish box

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let body = document.querySelector('body');
-
 let div = document.createElement('div');
 body.appendChild(div);
 
@@ -9,19 +8,21 @@ div.style.width = '288px'
 div.style.border = '8px solid red';
 div.style.flexWrap = 'wrap'
 
+let styleSheets = document.styleSheets;
+console.log(styleSheets);
 
 let frag = document.createDocumentFragment();
 
 for (i = 0; i < 256; i++){
   let div = document.createElement('div');
-  div.style.flex = '0 0 16px'
   div.classList.add('square')
-  div.style.width = '16px';
-  div.style.height = '16px';
-  div.style.backgroundColor = 'black';
-  div.style.border = '1px solid white';
   frag.appendChild(div);
 }
 
 div.appendChild(frag);
+
+let sheet = window.document.styleSheets[0];
+sheet.insertRule('.square {width: 16px; height: 16px; background-color: black; border: 1px solid white; flex: 0 0 16px}')
+sheet.insertRule('.square:hover {background-color: blue}');
+
 
