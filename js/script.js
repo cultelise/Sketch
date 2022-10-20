79f9e62a2686254dac7a6d4c4eabb15c592e46bf
elise <115508722+cultelise@users.noreply.github.com>
2022-10-20 09:14:59 -0500

    Complete v1

--- js/script.js
+++ js/script.js
@@ -2,18 +2,62 @@ let body = document.querySelector('body');
 let button = document.createElement('button');
 let div = document.createElement('div');
 
+let frag = document.createDocumentFragment();
+
+let answer = prompt('Enter number for grid size', '16');
+console.log(answer)
+guy(answer)
+
+let s = (16 * answer) + (answer * 2);
+
 body.appendChild(div);
 body.appendChild(button);
+body.style.justifyContent = 'center';
+body.style.flexDirection = 'column';
+body.style.gap = '20px';
+body.style.alignItems = 'center'
+body.style.display = 'flex';
 div.style.height = '256px'
 div.style.width = '256px'
 
+div.style.height = `${s}px`
+div.style.width = `${s}px`
+
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);}
+  }
+  
 button.textContent = 'Reset'
-button.addEventListener('click', (e) => {
-  let answer = prompt('Enter number for grid size', '16');
-  let s = (16 * answer) + (answer * 2);
-  div.style.height = `${s}px`
-  div.style.width = `${s}px`
+button.addEventListener('click', () => {
+  removeElementsByClass('square');
+  console.log()
+  let a1 = prompt('what?');
+  let s = (16 * a1) + (a1* 2);
+  div.style.height = `${s}px`;
+  div.style.width = `${s}px`;
+  guy(a1);
+})
+
+
+div.style.display = 'flex'
+div.style.alignSelf = 'center'
+div.style.border = '8px solid red';
+div.style.flexWrap = 'wrap'
+
+button.style.color = 'white';
+button.style.backgroundColor = 'black'
+button.style.fontSize = '24px'
+button.style.length = '100px'
+button.style.width = '100px'
+
+let div2 = document.querySelector('div2')
+
+
+function guy(answer) {  
   for (i = 0; i < (answer * answer); i++){
+    console.log(answer)
     let div2 = document.createElement('div2');
     div2.style.flex = '0 0 16px'
     div2.classList.add('square')
@@ -24,21 +68,7 @@ button.addEventListener('click', (e) => {
     frag.appendChild(div2);
     div2.addEventListener('mouseover', (e) => {
       e.target.style.backgroundColor = 'purple';
-    })
-  } div.appendChild(frag);
-})
-
-
-div.style.display = 'flex'
-
-div.style.border = '8px solid red';
-div.style.flexWrap = 'wrap'
-
-
-let frag = document.createDocumentFragment();
-
-
-let div2 = document.querySelector('div2')
-
-
+    }) 
+  } div.appendChild(frag); 
+}
 
